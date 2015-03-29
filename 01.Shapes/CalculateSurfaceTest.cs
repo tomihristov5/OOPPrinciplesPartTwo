1cef3649dac696a863669d1485bf2e14fe2a5d14
tomihristov5 <hrist0v@abv.bg>
2015-03-30 01:04:56 +0300

    update

---
+++ 01.Shapes/CalculateSurfaceTest.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.Shapes
+{
+    using System;
+    using System.Collections;
+
+    public static class CalculateSurfaceTest
+    {
+        public static void Main()
+        {
+            Shape[] shapes = 
+            {
+                new Rectangle(3.15, 4.3),
+                new Triangle(4, 5.6),
+                new Square(7.16)
+            };
+
+            foreach (var item in shapes)
+            {
+                Console.WriteLine("Shape is {0}, surface is: {1}", item.GetType().Name, item.CalculateSurface());
+            }
+        }
+    }
+}
