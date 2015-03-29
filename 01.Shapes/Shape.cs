04317893b986e28c2a688d8c29780041721d5700
tomihristov5 <hrist0v@abv.bg>
2015-03-30 01:03:03 +0300

    problem 1 added

---
+++ 01.Shapes/Shape.cs
@@ -0,0 +1,50 @@
+﻿namespace _01.Shapes
+{
+    public abstract class Shape
+    {
+        private double width;
+        private double height;
+
+        public Shape(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new System.ArgumentOutOfRangeException("Both width ana height must be possitive");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new System.ArgumentOutOfRangeException("Both width ana height must be possitive");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public abstract double CalculateSurface();
+    }
+}
---
+++ 01.Shapes/Square.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.Shapes
+{
+    public class Square : Shape
+    {
+        public Square(double side)
+            : base(side, side)
+        {
+            
+        }
+
+        public override double CalculateSurface()
+        {
+            return this.Width * this.Width;
+        }
+    }
+}
