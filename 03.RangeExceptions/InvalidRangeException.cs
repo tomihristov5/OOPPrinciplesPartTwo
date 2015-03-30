96580abca723820f3c4e84b0bc40eb4df69eafe8
tomihristov5 <hrist0v@abv.bg>
2015-03-30 16:14:57 +0300

    final

---
+++ 03.RangeExceptions/InvalidRangeException.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.RangeExceptions
+{
+    using System;
+
+    public class InvalidRangeException<T> : ApplicationException
+    {
+        public InvalidRangeException(string msg, T start, T end)
+            : base(msg)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        public T Start { get; set; }
+
+        public T End { get; set; }
+    }
+}
